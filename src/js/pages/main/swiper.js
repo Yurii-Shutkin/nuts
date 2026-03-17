1eed74ae34eae2dd014001c188db1ca2344dd060
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-03-17 15:12:48 +0200

    added swiper to manufactures block

---
+++ src/js/pages/main/swiper.js
@@ -0,0 +1,13 @@
+import Swiper from "swiper";
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+const swiper = new Swiper('.manufacturer__swiper', {
+  modules: [Navigation],
+  loop: true,
+   navigation: {
+    nextEl: '.manufacturer__slider-icon--right',
+    prevEl: '.manufacturer__slider-icon--left',
+  },
+});
