bc9b7678944fba4b09d2ad3ac54643070f613163
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-03-20 14:24:21 +0200

    fixed sidebar bugs

--- src/js/partials/burgerSide.js
+++ src/js/partials/burgerSide.js
@@ -1,12 +1,14 @@
+const body = document.querySelector('body');
 const burger = document.querySelector('.burger');
 const sidebar = document.querySelector('.sidebar');
 const sidebarCloseBtn = document.querySelector('.sidebar__close-btn');
 
 burger.addEventListener('click', () => {
   sidebar.style.transform = 'translateX(0)';
-  console.log('clicked')
+  body.style.overflow = 'hidden';
 })
 
 sidebarCloseBtn.addEventListener('click', () => {
   sidebar.style.transform = 'translateX(-100%)';
+  body.style.overflow = 'auto';
 })
