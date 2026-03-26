e33584e2dac65baa5d830fd87c0530068fc4151c
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-03-26 14:31:52 +0200

    finished news-and-posts page adaptive layout

---
+++ src/js/pages/news-and-posts/main.js
@@ -0,0 +1,7 @@
+import 'virtual:svg-icons-register';
+import '@/js/partials/burgerSide.js';
+import '@/js/partials/select.js';
+import '@/js/partials/loadMap.js';
+import '@/js/partials/galleryVideoHandler.js';
+
+
