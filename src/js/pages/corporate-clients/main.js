4428930b3dd94b6204ae65d6cc0099a736758081
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-03-26 11:28:12 +0200

    fixed some bugs

--- src/js/pages/corporate-clients/main.js
+++ src/js/pages/corporate-clients/main.js
@@ -1,4 +1,6 @@
 import 'virtual:svg-icons-register';
+import '@/js/partials/burgerSide.js';
+import '@/js/partials/select.js';
 import '@/js/partials/loadMap.js';
 import '@/js/partials/ecoHeroSlider.js';
 import '@/js/partials/tabsSwiper.js';
--- src/js/pages/payment-and-delivery/main.js
+++ src/js/pages/payment-and-delivery/main.js
@@ -1,5 +1,7 @@
 import 'virtual:svg-icons-register';
 import '@/js/partials/loadMap.js';
+import '@/js/partials/burgerSide.js';
+import '@/js/partials/select.js';
 import '@/js/partials/tabsSwiper.js';
 import '@/js/partials/videoBlock.js';
 
