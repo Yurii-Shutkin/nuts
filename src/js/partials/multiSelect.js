2731336f5f72e9153ddcdbe742181fa28c71e632
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-03-30 17:35:25 +0300

    fixed bugs wits multiselect

--- src/js/partials/multiSelect.js
+++ src/js/partials/multiSelect.js
@@ -1,21 +1,11 @@
-const multiselect = document.querySelector(".form__multiselect-control");
-const multiselectArrow = document.querySelector(".form__multiselect-arrow");
-const multiselectValue = document.querySelector(".form__multiselect-value");
-const multiselectDropdown = document.querySelector(".form__multiselect-dropdown");
-const options = document.querySelectorAll(".form__multiselect-option");
+document.querySelectorAll(".form__multiselect").forEach((multiselect) => {
+  const control = multiselect.querySelector(".form__multiselect-control");
+  const arrow = multiselect.querySelector(".form__multiselect-arrow");
+  const dropdown = multiselect.querySelector(".form__multiselect-dropdown");
 
-multiselect.addEventListener("click", () => {
-  multiselect.classList.toggle("form__multiselect-control--active");
-  multiselectDropdown.classList.toggle("form__multiselect-dropdown--active");
-  multiselectArrow.classList.toggle("form__multiselect-arrow--active");
+  control.addEventListener("click", () => {
+    control.classList.toggle("form__multiselect-control--active");
+    dropdown.classList.toggle("form__multiselect-dropdown--active");
+    arrow.classList.toggle("form__multiselect-arrow--active");
+  });
 });
-
-multiselectDropdown.addEventListener("click", (e) => {
-  e.stopPropagation();
-  const option = e.target.closest(".form__multiselect-option");
-  if (!option) return;
-  if (e.target.tagName === 'INPUT') return;
-  option.classList.toggle("form__multiselect-option--active");
-});
-
-
