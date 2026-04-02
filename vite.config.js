b8e9d093ece4fe9cec17fc0df7170af55008c73f
Yurii-Shutkin <yurashutkinn@gmail.com>
2026-04-02 14:16:15 +0300

    fixed paths from dev adn prod modes

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,11 @@ import { hulakPlugins } from 'vite-plugin-hulak-tools';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const base = process.env.NODE_ENV === 'production' ? '/nuts/' : '/';
+
 export default defineConfig({
-  base: '/',
+  base: base,
+
   server: {
     port: 3000,
     open: true
@@ -18,7 +21,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 
@@ -26,56 +29,68 @@ export default defineConfig({
     createHtmlPlugin(),
 
     hulakPlugins({
-      enableHandlebars: true,
-      handlebarsOptions: {
-        partialDirectory: 'src/html/partials'
-      },
+      enableHandlebars: false,
     }),
 
     createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'public/img/icons/')],
+      iconDirs: [path.resolve(process.cwd(), './public/img/icons/')],
       symbolId: 'icon-[name]',
     }),
 
     handlebars({
-        reloadOnPartialChange: true,
-        partialDirectory: [path.resolve(__dirname, "src/html/")],
-        helpers: {
-          array(...args) {
-            return args.slice(0, -1);
-          },
-          dict(...args) {
-            const obj = {};
-            for (let i = 0; i < args.length - 1; i += 2) {
-              obj[args[i]] = args[i + 1];
-            }
-            return obj;
-          },
-          dataAttrs(attrs) {
-            return Object.entries(attrs || {})
-              .map(([key, value]) => `data-${key}="${value}"`)
-              .join(" ");
-          },
+      reloadOnPartialChange: true,
+      partialDirectory: [path.resolve(__dirname, "./src/html/")],
+      helpers: {
+        array(...args) {
+          return args.slice(0, -1);
         },
-        context: {
+        dict(...args) {
+          const obj = {};
+          for (let i = 0; i < args.length - 1; i += 2) {
+            obj[args[i]] = args[i + 1];
+          }
+          return obj;
+        },
+        dataAttrs(attrs) {
+          return Object.entries(attrs || {})
+            .map(([key, value]) => `data-${key}="${value}"`)
+            .join(" ");
+        },
+         baseUrl(path) {
+          const urlPath = typeof path === 'string' ? path : '';
+          const cleanPath = urlPath.startsWith('/') ? urlPath.slice(1) : urlPath;
+          return `${base}${cleanPath}`;
+        },
 
-        },
-      }),
+      },
+      context: {},
+    }),
   ],
 
   build: {
     outDir: 'dist',
     sourcemap: true,
     minify: 'esbuild',
+
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, 'index.html'),
-        about: path.resolve(__dirname, '/pages/about/index.html'),
-        'payment-and-delivery': path.resolve(__dirname, "/pages/payment-and-delivery/index.html"),
-        'corporate-clients': path.resolve(__dirname, "/pages/corporate-clients/index.html"),
-        'news-and-posts': path.resolve(__dirname, "/pages/news-and-posts/index.html"),
-        'contact-info': path.resolve(__dirname, "/pages/contact-info/index.html"),
+        main: path.resolve(__dirname, './index.html'),
+        about: path.resolve(__dirname, './pages/about/index.html'),
+        order: path.resolve(__dirname, "./pages/order/index.html"),
+        cart: path.resolve(__dirname, "./pages/cart/index.html"),
+        catalog: path.resolve(__dirname, "./pages/catalog/index.html"),
+        gallery: path.resolve(__dirname, "./pages/gallery/index.html"),
+        'payment-and-delivery': path.resolve(__dirname, "./pages/payment-and-delivery/index.html"),
+        'corporate-clients': path.resolve(__dirname, "./pages/corporate-clients/index.html"),
+        'news-and-posts': path.resolve(__dirname, "./pages/news-and-posts/index.html"),
+        'contact-info': path.resolve(__dirname, "./pages/contact-info/index.html"),
+        'one-news': path.resolve(__dirname, "./pages/one-news/index.html"),
+        'order-redirect': path.resolve(__dirname, "./pages/order-redirect/index.html"),
+        'password-recovery': path.resolve(__dirname, "./pages/password-recovery/index.html"),
+        'product-card': path.resolve(__dirname, "./pages/product-card/index.html"),
+        'sign-in': path.resolve(__dirname, "./pages/sign-in/index.html"),
+        'sign-up': path.resolve(__dirname, "./pages/sign-up/index.html"),
       },
     },
   }
-})
+});
